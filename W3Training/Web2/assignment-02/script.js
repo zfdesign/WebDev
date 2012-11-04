e51a3f7128a0c0d0ac583e4c92c73f298e1addea
ZFDesign <zlatinivanov01@gmail.com>
2012-11-03 19:52:44 -0700

    Introducing NameSpace and Private and Public methods

--- W3Training/Web2/assignment-02/script.js
+++ W3Training/Web2/assignment-02/script.js
@@ -1,12 +1,28 @@
-var entries = document.getElementsByClassName('entries'),
-	articles = entries[0].getElementsByTagName('article');
-for(i=0 ; i<articles.length ; i++){
-	articles[i].addEventListener('click', function(e){
-		var element = e.target;
-		if (element.nodeName.toLowerCase() === 'h2') {
-			var pNode = element.parentNode;
+/* set 'hidden' className onLoad */
+var appNS = (function() {
+	var entries = document.getElementsByClassName('entries'),
+		articles = entries[0].getElementsByTagName('article')
+		aLength = articles.length,
+		toggleParentClass = function(e) {
+			var pNode = e.target.parentNode;
 			if ( pNode.className.length > 0 ) { pNode.className = ""; } 
 			else { pNode.className = "show"; }
+		},
+		addEvents = function() {
+			for( i=0; i < aLength; i++ ) {
+				var h2 = articles[i].getElementsByTagName('h2');
+				h2[0].addEventListener('click', toggleParentClass, false);
+			}
+		},
+		removeEvents = function() {
+			for( i=0; i < aLength; i++ ) {
+				var h2 = articles[i].getElementsByTagName('h2');
+				h2[0].removeEventListener('click', toggleParentClass, false);
+			}
+		};
+		return { 
+			addEvents: addEvents,
+			removeEvents: removeEvents
 		}
-	}, false);
-}
+})();
+window.addEventListener('load', addEvents, false);
