3044b23e2d1b2271f6bbcce3f2277bc7236eecaf
ZFDesign <zlatinivanov01@gmail.com>
2012-11-03 19:57:01 -0700

    Toggle open articles, to show only one active

--- W3Training/Web2/assignment-02/script.js
+++ W3Training/Web2/assignment-02/script.js
@@ -4,18 +4,23 @@ var appNS = (function() {
 		articles = entries[0].getElementsByTagName('article')
 		aLength = articles.length,
 		toggleParentClass = function(e) {
-			var pNode = e.target.parentNode;
+			var pNode = e.target.parentNode,
+				showing = document.getElementsByClassName('show'),
+				showingLength = showing.length, j;
+			for (j = 0; j < showingLength; j++) { 
+				if ( showing[j] !== e.target ) showing[j].className = ""; 
+			}
 			if ( pNode.className.length > 0 ) { pNode.className = ""; } 
 			else { pNode.className = "show"; }
 		},
 		addEvents = function() {
-			for( i=0; i < aLength; i++ ) {
+			for( var i=0; i < aLength; i++ ) {
 				var h2 = articles[i].getElementsByTagName('h2');
 				h2[0].addEventListener('click', toggleParentClass, false);
 			}
 		},
 		removeEvents = function() {
-			for( i=0; i < aLength; i++ ) {
+			for( var i=0; i < aLength; i++ ) {
 				var h2 = articles[i].getElementsByTagName('h2');
 				h2[0].removeEventListener('click', toggleParentClass, false);
 			}
