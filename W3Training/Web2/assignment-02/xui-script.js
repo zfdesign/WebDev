5df880280352845631d6f22dbf06daec70e0e6c1
ZFDesign <zlatinivanov01@gmail.com>
2012-11-09 16:32:22 -0800

    Removed redundant classes, Updated Styles and Added toggle all articles functionality

--- W3Training/Web2/assignment-02/xui-script.js
+++ W3Training/Web2/assignment-02/xui-script.js
@@ -1,8 +1,18 @@
-var Xarticle = x$('.entries').find('article'), Xclass = 'hide';
-
-Xarticle.each(function(){ x$(this).addClass(Xclass); });
+var Xarticles = x$('.entries').find('article'), Xclass = 'hide', hideAll = false;
+Xarticles.each(function(){ x$(this).addClass(Xclass); });
 x$('article > h2').on('click', function (e) {
 	var pNode = x$(this)[0].parentNode;
-	Xarticle.not(pNode).each(function(){ x$(this).addClass(Xclass); }); // optional collapse all other
 	x$(pNode).toggleClass(Xclass);
+	Xarticles.not(pNode).each(function(){ 
+		x$(this).addClass(Xclass); 
+		hideAll = false;
+		x$('body > h1').removeClass('showingAll');
+	}); 
+});
+x$('body > h1').on('click', function () {
+	Xarticles.each( function(e, i) { 
+		(hideAll) ? x$(this).addClass(Xclass) : x$(this).removeClass(Xclass); 
+	});
+	x$(this).toggleClass('showingAll');
+	hideAll = (hideAll) ? false : true;	
 });
