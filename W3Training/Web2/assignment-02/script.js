652ab59f06acdf40c3f99d489736a4b03db91c84
ZFDesign <zlatinivanov01@gmail.com>
2012-11-14 11:53:41 -0800

    Added toggle all functionality

--- W3Training/Web2/assignment-02/script.js
+++ W3Training/Web2/assignment-02/script.js
@@ -1,15 +1,19 @@
-var appNS = (function() {
-	var entries = document.getElementsByClassName('entries'),
-		articles = entries[0].getElementsByTagName('article'),
+var appNS = (function () {
+	var heading = document.getElementsByTagName('h1')[0],
+		entries = document.getElementsByClassName('entries')[0],
+		articles = entries.getElementsByTagName('article'),
 		aLength = articles.length,
+		headingClass= 'showingAll', 
 		activeClass = 'show',
-		addClass = function(clss, el) {
+		
+		addClass = function (clss, el) {
 			if ( el === undefined ) el = this; 
-			if ( el.className !== undefined ) { 
+			if ( el.className !== undefined && el.className.indexOf(clss) === -1 ) { 
 				var clssVal = (el.className.length > 0) ? ' ' + clss : clss;
 				el.className += clssVal; 
 			}
 		},
+		
 		removeClass = function (clss, el) {
 			if ( el === undefined ) el = this; 
 			if ( el.className !== undefined && el.className.indexOf(clss) !== -1) {
@@ -17,14 +21,15 @@ var appNS = (function() {
 				el.className = ( val.length > 0 ) ? val.replace(/^\s+|\s+$/, '') : val;
 			}
 		},
-		toggleParentClass = function(e) {
+		
+		toggleParentClass = function (e) {
 			var pNode = e.target.parentNode,
-				showing = entries[0].getElementsByClassName(activeClass),
+				showing = entries.getElementsByClassName(activeClass),
 				showingLength = showing.length, j;
 
 			if ( showingLength > 0 ) {
 				// Hide all, but current
-				for (j = 0; j < showingLength; j++) { 
+				for ( j = 0; j < showingLength; j++ ) { 
 					if ( showing[j] !== pNode ) {
 						removeClass(activeClass, showing[j]); 
 					}
@@ -33,44 +38,55 @@ var appNS = (function() {
 			// Toggle
 			if ( pNode.className.indexOf(activeClass) !== -1 ) { 
 				removeClass(activeClass, pNode); 
+				removeClass(headingClass, heading); 
 			} else { 
 				addClass(activeClass, pNode);
 			}
 		},
-		addH2Events = function() {
-			for( var i=0; i < aLength; i++ ) {
+		
+		addH2Events = function () {
+			for ( var i=0; i < aLength; i++ ) {
 				var h2 = articles[i].getElementsByTagName('h2');
 				h2[0].addEventListener('click', toggleParentClass, false);
 			}
 		},
-		removeH2Events = function() {
-			for( var i=0; i < aLength; i++ ) {
+		
+		removeH2Events = function () {
+			for ( var i=0; i < aLength; i++ ) {
 				var h2 = articles[i].getElementsByTagName('h2');
 				h2[0].removeEventListener('click', toggleParentClass, false);
 			}
 		},
-		setToggleAllEvent = function(element){
-			element.addEventListener('click', function() {
+		
+		setToggleAllEvent = function (element) {
+			element.addEventListener('click', function () {
 				var articlesShowing = entries.getElementsByClassName(activeClass); 
-				if (articlesShowing.length > 0) {
-					for (var i = 0; i < articlesShowing.length; i++) {
-						articlesShowing[i].className.replace(activeClass, '');
+
+				if ( heading.className.indexOf(headingClass) !== -1 ) {
+					for ( var i = 0; i < aLength; i++ ) {
+						removeClass(activeClass, articles[i]);
 					}
-				} else {
-					for (var k = 0; k < articlesShowing.length; k++) {
-						addClass(activeClass, articles[k]);
+					removeClass(headingClass, heading);
+				} else { 
+					for ( var j = 0; j < aLength; j++ ) {
+						addClass(activeClass, articles[j]);
 					}
+					addClass(headingClass, heading);
 				}
+
 			}, false);
 		}, 
-		init = function() {
+		
+		init = function () {
 			addH2Events();
-			//setToggleAllEvent();
+			setToggleAllEvent(heading);
 		};
 		return { 
-			addClass: addClass,
-			addH2Events: addH2Events,
-			removeH2Events: removeH2Events
+			init: init, 
+			addClass: addClass, 
+			removeClass: removeClass, 
+			addH2Events: addH2Events, 
+			removeH2Events: removeH2Events 
 		}
 })();
-window.addEventListener('load', addH2Events, false);
+window.addEventListener('load', appNS.init, false);
