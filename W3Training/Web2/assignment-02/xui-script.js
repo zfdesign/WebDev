aaaa05989398d77de4a6d313425182a8b348932a
ZFDesign <zlatinivanov01@gmail.com>
2012-11-21 07:13:59 -0800

    Styling the app

--- W3Training/Web2/assignment-02/xui-script.js
+++ W3Training/Web2/assignment-02/xui-script.js
@@ -1,18 +1,45 @@
-var Xarticles = x$('.entries').find('article'), Xclass = 'hide', hideAll = false;
-Xarticles.each(function(){ x$(this).addClass(Xclass); });
-x$('article > h2').on('click', function (e) {
-	var pNode = x$(this)[0].parentNode;
-	x$(pNode).toggleClass(Xclass);
-	Xarticles.not(pNode).each(function(){ 
-		x$(this).addClass(Xclass); 
+(function() {
+	var Xarticles = x$('.entries').find('article'), 
+		Xclass = 'hide', 
 		hideAll = false;
-		x$('body > h1').removeClass('showingAll');
-	}); 
-});
-x$('body > h1').on('click', function () {
-	Xarticles.each( function(e, i) { 
-		(hideAll) ? x$(this).addClass(Xclass) : x$(this).removeClass(Xclass); 
+
+	/* Toggle notes content */ 
+	Xarticles.each(function(){ x$(this).addClass(Xclass); });
+	x$('article > h2').on('click', function (e) {
+		var pNode = x$(this)[0].parentNode;
+		x$(pNode).toggleClass(Xclass);
+		Xarticles.not(pNode).each(function(){ 
+			x$(this).addClass(Xclass); 
+			hideAll = false;
+			x$('body > h1').removeClass('showingAll');
+		}); 
 	});
-	x$(this).toggleClass('showingAll');
-	hideAll = (hideAll) ? false : true;	
-});
+	x$('h1').on('click', function () {
+		Xarticles.each( function(e, i) { 
+			(hideAll) ? x$(this).addClass(Xclass) : x$(this).removeClass(Xclass); 
+		});
+		x$(this).toggleClass('showingAll');
+		hideAll = (hideAll) ? false : true;	
+	});
+
+	/* Toggle Menu visibility */
+	x$('a.menuLink').on('click', function(e) {
+		e.preventDefault();
+		x$('body').toggleClass('menuOn');
+		x$('nav > ul > li').addClass('hideOptions');
+	});
+	x$('.entriesOverlay').on('click', function(e) {
+		e.preventDefault();
+		x$('body').removeClass('menuOn');
+		x$('#Options').addClass('hideOptions');
+	});
+
+	/* Toggle expandable menu options */
+	x$('nav > ul > li').addClass('hideOptions');
+	x$('nav > ul > li > a').on('click', function(e) {
+		e.preventDefault();
+		var liNode = x$(this)[0].parentNode;
+		x$(liNode).toggleClass('hideOptions');
+	});
+})();
+
